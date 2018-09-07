146640b016d4d386f5343776a258dd8ae69b92c7
Julian Haeger <julianhaeger@gmail.com>
2018-09-07 11:29:06 +0100

    remove dependency on blessed.js for interactive filtering todo picker

--- src/cli/todoPicker.ts
+++ src/cli/todoPicker.ts
@@ -1,63 +1,11 @@
-import * as blessed from 'blessed'
 import { TodoTree, deepFilterAll, TreeNode } from '../core/tree'
-import { Todo } from '../core/todo'
 import { renderTodoTree } from './renderers'
 import { Predicate } from '../core/predicate'
 import { isUndefined } from 'util'
 import { flatMap } from 'lodash'
-import { reject } from 'bluebird'
+import { clearScreen } from 'ansi-escapes';
 
-function setup () {
-  const screen = blessed.screen({
-    title: 'hello world',
-    smartCSR: true,
-    dockBorders: false,
-    fullUnicode: true,
-    autoPadding: true
-  })
-
-  const treeControl = blessed.text({
-    top: 0,
-    left: 0,
-    height: '80%',
-    width: '100%',
-    scrollable: true
-  })
-
-  const log = blessed.log({ height: 1, width: '100%' })
-
-  const input = blessed.textbox({
-    bottom: 0,
-    left: 0,
-    height: 1,
-    width: '100%',
-    keys: true,
-    mouse: true,
-    inputOnFocus: true,
-    style: {
-      fg: 'white',
-      bg: 'blue'	// Blue background so you see this is different from body
-    }
-  })
-
-  const wrapper = blessed.layout({
-    layout: 'grid',
-    width: '100%',
-    height: '100%'
-  })
-  wrapper.append(treeControl)
-  wrapper.append(log)
-  wrapper.append(input)
-  screen.append(wrapper)
-
-  screen.render()
-  input.focus()
-
-  return { screen, treeControl, input, log }
-
-}
-
-function applyFilter (filter: RegExp | undefined, trees: TodoTree[]): TodoTree[] {
+function applyFilter(filter: RegExp | undefined, trees: TodoTree[]): TodoTree[] {
   if (isUndefined(filter)) {
     return trees
   }
@@ -70,62 +18,67 @@ function applyFilter (filter: RegExp | undefined, trees: TodoTree[]): TodoTree[]
   return deepFilterAll(trees, predicate)
 }
 
-export function interactivePicker (trees: TodoTree[]): Promise<TodoTree> {
+export function interactivePicker(trees: TodoTree[]): Promise<TodoTree> {
   return new Promise((resolve) => {
-    const { treeControl, input, screen, log } = setup()
-    const setContent = (trees: TodoTree[], showNumbers = false) => {
+    process.stdin.setRawMode!(true)
+    process.stdin.setEncoding('utf8')
+
+    const drawTrees = (trees: TodoTree[], showNumbers = false) => {
       const rendered = renderTodoTree(trees, showNumbers)
-      treeControl.setContent(rendered)
-      screen.render()
+      process.stdout.write(clearScreen)
+      console.log(rendered)
+    }
+    const drawTreesWithPrompt = (trees: TodoTree[], prompt: string) => {
+      drawTrees(trees)
+      console.log()
+      process.stdout.write(prompt)
     }
 
     let filter: RegExp | undefined = undefined
+    let filterString = ''
     let filteredTrees: TodoTree[] = trees
-    let done = false
-    input.on('keypress', () => {
-      setImmediate(() => {
-        if (done) {
-          console.log('keypress firing')
-          return
-        }
-        if (input.content.trim() === '') {
-          log.add('filter = none')
-          filter = undefined
-        } else {
-          log.add(`filter = ${input.content}`)
-          filter = new RegExp(input.content)
-        }
-        filteredTrees = applyFilter(filter, trees)
-        setContent(filteredTrees)
-      })
-    })
-    setContent(filteredTrees)
 
-    input.on('submit', () => {
-      done = true
-      screen.remove(input)
-      input.removeAllListeners()
-      input.destroy()
+    const processKey = (key: string) => {
+      if (key === '\u0003') {
+        process.exit();
+      }
 
-      screen.remove(treeControl)
-      treeControl.destroy()
-      log.destroy()
+      if (key.charCodeAt(0) === 13) {
+        process.stdin.setRawMode!(false)
+        process.stdin.removeListener('data', processKey)
+        process.stdout.write(clearScreen)
+        resolve(getFinalResult(filteredTrees))
+        return
+      }
 
-      screen.removeAllListeners()
-      screen.detach()
-      screen.destroy()
-      setImmediate(() => {
-        const leaves = flatMap(filteredTrees, toLeafList)
-        if (leaves.length === 1) {
-          return resolve(leaves[0])
-        }
-        return reject(new Error('ambiguous selection'))
-      })
-    })
+      if(key.charCodeAt(0) === 127){
+        filterString = filterString.slice(0,-1)
+      } else {
+        filterString = (filterString || '') + key
+      }
+
+      filter = new RegExp(filterString || '.*')
+      filteredTrees = applyFilter(filter, trees)
+      const prompt = 'filter: ' + filterString
+      drawTreesWithPrompt(filteredTrees, prompt)
+      return
+    }
+    process.stdin.on('data', processKey)
+
+    const prompt = 'filter: ' + filterString
+    drawTreesWithPrompt(filteredTrees, prompt)
   })
 }
 
-function toLeafList<T> (tree: TreeNode<T>): TreeNode<T>[] {
+function getFinalResult(filteredTrees: TodoTree[]) {
+  const leaves = flatMap(filteredTrees, toLeafList);
+  if (leaves.length !== 1) {
+    throw new Error('ambiguous selection');
+  }
+  return leaves[0];
+}
+
+function toLeafList<T>(tree: TreeNode<T>): TreeNode<T>[] {
   if (tree.children.length === 0) {
     return [tree]
   }
